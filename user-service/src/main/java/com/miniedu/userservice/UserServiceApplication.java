a731072baef59d7ff5611d6358b52c9cea4411fd
Pete Chiboleca <pete9450@gmail.com>
2026-06-16 23:37:43 +0200

    Align application.yml to work with environment variables
    Remove unnecessary Jasypt encryption dependence

--- user-service/src/main/java/com/miniedu/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/miniedu/userservice/UserServiceApplication.java
@@ -1,13 +1,11 @@
 package com.miniedu.userservice;
 
 import com.miniedu.userservice.kafka.config.KafkaTopicProperties;
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 @EnableConfigurationProperties(KafkaTopicProperties.class)
 public class UserServiceApplication {
 
